1461e4a1e2e456d8a93fae86ff9a79256ba4304c
styceplug <106968888+styceplug@users.noreply.github.com>
2024-09-18 14:48:55 +0100

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -3,13 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDu9ip2cN_VCbUv9dHeYjUo_JflaIsakcg",
-    authDomain: "escrow-f5933.firebaseapp.com",
-    projectId: "escrow-f5933",
-    storageBucket: "escrow-f5933.appspot.com",
-    messagingSenderId: "1009282368630",
-    appId: "1:1009282368630:web:97a033a6df370d30770b57",
-    measurementId: "G-RN7HRYBTNY"
+    
   };
 
 // Initialize Firebase
